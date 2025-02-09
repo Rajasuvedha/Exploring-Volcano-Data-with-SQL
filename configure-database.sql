1d224f8727b848d64a63a7c3f6048fe99451ab39
Rajasuvedha <146409086+Rajasuvedha@users.noreply.github.com>
2025-02-09 18:02:48 +0800

    Delete configure-database.sql

--- configure-database.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS volcano_eruptions;
-CREATE DATABASE volcano_eruptions;
-
-USE volcano_eruptions;
-
-DROP USER IF EXISTS 'user'@'%';
-CREATE USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'singapore'; 
-GRANT ALL ON volcano_eruptions.* TO 'user'@'%';
